8b85d4c4d2586db93eb265bd3760d33abcb3f596
Jack He <jackhe04@gmail.com>
2023-08-02 14:06:07 +0800

    main function

--- reader.py
+++ reader.py
@@ -3,6 +3,7 @@ import os
 import json
 import re
 import time
+import datetime
 
 
 def read_json(directory: str):
@@ -15,9 +16,15 @@ def read_json(directory: str):
     valid_files = {}
     for file in json_files:
         if matsuri_pattern.match(file) is not None:
-            valid_files[re.findall(r"\d{13}(?=\.json)", file)[0]] = [file, "matsuri"]
+            liver = re.findall(r".+(?=_.+_\d{13}\.json)", file)[0]
+            valid_files[re.findall(r"\d{13}(?=\.json)", file)[0]] = [
+                file,
+                "matsuri",
+                liver,
+            ]
         elif danmakus_pattern.match(file) is not None:
-            valid_files[re.findall(r"\d{13}", file)[1]] = [file, "danmakus"]
+            liver = re.findall(r"(?<=\d_).+(?=\.json)", file)[0]
+            valid_files[re.findall(r"\d{13}", file)[1]] = [file, "danmakus", liver]
     if len(valid_files) == 0:
         raise OSError("没有找到合法的弹幕文件")
 
@@ -28,37 +35,43 @@ def read_json(directory: str):
 
     with open(os.path.join(directory, valid_files[recent][0]), encoding="utf8") as file:
         try:
-            return {"data": json.load(file), "type": valid_files[recent][1]}
+            return {
+                "data": json.load(file),
+                "type": valid_files[recent][1],
+                "start_time": recent,
+                "liver": valid_files[recent][2],
+            }
         except json.JSONDecodeError:
             raise OSError("不合法的json文件")
 
+
 def parse_matsuri(data: object):
-    danmu_rows = ''
-    for i in data['full_comments']:
-        dm_time = i['time'] - data['info']['start_time']
-        text = ''
-        if 'superchat_price' in i.keys():
+    danmu_rows = ""
+    for i in data["full_comments"]:
+        dm_time = i["time"] - data["info"]["start_time"]
+        text = ""
+        if "superchat_price" in i.keys():
             text = f"￥{i['superchat_price']} <b>{i['text']}</b>"
-        elif 'text' in i.keys():
-            if '点点红包抽礼物' in i['text']:
+        elif "text" in i.keys():
+            if "点点红包抽礼物" in i["text"]:
                 continue
-            text = i['text']
-        elif 'gift_name' in i.keys():
+            text = i["text"]
+        elif "gift_name" in i.keys():
             text = f"￥{i['gift_price']} <b>{i['gift_name']} * {i['gift_num']}</b>"
-        
-        danmu_rows += f'<tr><td>{time.strftime("%H:%M:%S", time.localtime(dm_time // 1000))}.{dm_time % 1000}</td><td><a target="_blank" href="https://space.bilibili.com/{i["user_id"]}">${i["username"]}</a></td><td>${text}</td></tr>'
-    
-    return f'''<!DOCTYPE html>
+
+        danmu_rows += f'<tr><td>{datetime.datetime.fromtimestamp(dm_time // 1000, datetime.timezone.utc).strftime("%H:%M:%S")}.{dm_time % 1000}</td><td><a target="_blank" href="https://space.bilibili.com/{i["user_id"]}">{i["username"]}</a></td><td>{text}</td></tr>'
+
+    return f"""<!DOCTYPE html>
 <html>
 
 <head>
-    <title>{time.strftime("%Y.%m.%d", time.localtime(data['info']['start_time']))}_直播弹幕文件</title>
+    <title>{time.strftime("%Y%m%d", time.localtime(data['info']['start_time'] // 1000))}_直播弹幕文件</title>
     <link rel="stylesheet" href="../table_style.css">
 </head>
 
 <body>
 
-    <p>开始时间：{time.strftime("%Y.%m.%d %H:%M:%S", time.localtime(data['info']['start_time']))}</p>
+    <p>开始时间：{time.strftime("%Y.%m.%d %H:%M:%S", time.localtime(data['info']['start_time'] // 1000))}</p>
     <br><br>
     <table>
         <tr>
@@ -71,10 +84,67 @@ def parse_matsuri(data: object):
 
 </body>
 
-</html>'''
+</html>"""
+
+
+def parse_danmakus(data: object):
+    danmu_rows = ""
+    for i in data["danmakus"]:
+        dm_time = i["sendDate"] - data["live"]["startDate"]
+        text = ""
+        if i["type"] == 0:
+            if re.match(r"\[http", i["message"]) is not None:
+                text = f'<img src="{i["message"][1:-1]}" alt="图片表情" height="50">'
+            else:
+                text = i["message"]
+        elif i["type"] == 1 or i["type"] == 2 or i["type"] == 3:
+            if i["price"] == 0:
+                continue
+            text = f"￥{i['price']} <b>{i['message']}</b>"
+
+        danmu_rows += f'<tr><td>{datetime.datetime.fromtimestamp(dm_time // 1000, datetime.timezone.utc).strftime("%H:%M:%S")}.{dm_time % 1000}</td><td><a target="_blank" href="https://space.bilibili.com/{i["uId"]}">{i["uName"]}</a></td><td>{text}</td></tr>'
+
+    return f"""<!DOCTYPE html>
+<html>
+
+<head>
+    <title>{time.strftime("%Y%m%d", time.localtime(data['live']["startDate"] // 1000))}_直播弹幕文件</title>
+    <link rel="stylesheet" href="../table_style.css">
+</head>
+
+<body>
+
+    <p>开始时间：{time.strftime("%Y.%m.%d %H:%M:%S", time.localtime(data['live']["startDate"] // 1000))}</p>
+    <br><br>
+    <table>
+        <tr>
+            <th>时间</th>
+            <th>用户</th>
+            <th>内容</th>
+        </tr>
+        {danmu_rows}
+    </table>
+
+</body>
+
+</html>"""
 
 
 if __name__ == "__main__":
-    result = read_json(os.getcwd() + "/test_input")
-    if result['type'] == 'matsuri':
-        parse_matsuri(result['data'])
+    result = read_json(os.getcwd())
+    start_time = time.strftime(
+        "%Y%m%d-%H%M%S", time.localtime(int(result["start_time"]) // 1000)
+    )
+    output_html = ""
+    if result["type"] == "matsuri":
+        output_html = parse_matsuri(result["data"])
+    else:
+        output_html = parse_danmakus(result["data"])
+
+    if not os.path.exists("output"):
+        os.makedirs("output")
+    filename = f'output/{start_time}-{result["liver"]}.html'
+    with open(filename, "w", encoding="utf8") as file:
+        file.write(output_html)
+    print("文件已生成:", os.path.join(os.getcwd(), filename))
+    webbrowser.open(f"file:///{os.path.join(os.getcwd(), filename)}")
